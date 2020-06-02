a742fed11bb3fd981ab1b740d1eda6ff0b862114
user <magi@magi-desktop>
2020-06-02 14:29:26 +0900

    ensure proper arn format for non-CIS stardards

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 CIS_STANDARD_RESOURCE = 'ruleset/cis-aws-foundations-benchmark/v/1.2.0'
 CIS_STANDARD_ARN = 'arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0'
-"arn:aws:securityhub:us-west-2::standards/pci-dss/v/3.2.1"
+#"arn:aws:securityhub:us-west-2::standards/pci-dss/v/3.2.1"
 def get_standard_arn_for_region_and_resource(region, standard_resource):
     if standard_resource == CIS_STANDARD_ARN or standard_resource == CIS_STANDARD_RESOURCE:
         return CIS_STANDARD_ARN
@@ -8,5 +8,5 @@ def get_standard_arn_for_region_and_resource(region, standard_resource):
         return 'arn:{partition}:securityhub:{region}::{resource}'.format(
             partition='aws',
             region=region,
-            resource=standard_resource.spit(':').pop()
+            resource=standard_resource.split(':').pop()
         )
