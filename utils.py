be56527af8701c477208180027de9e6968e4b72a
user <magi@magi-desktop>
2020-06-02 13:44:58 +0900

    add test script

--- utils.py
+++ utils.py
@@ -5,4 +5,8 @@ def get_standard_arn_for_region_and_resource(region, standard_resource):
     if standard_resource == CIS_STANDARD_ARN or standard_resource == CIS_STANDARD_RESOURCE:
         return CIS_STANDARD_ARN
     else:
-        return 'arn:{partition}:securityhub:{region}::{resource}'.format(partition='aws', region=region, resource=standard_resource)
+        return 'arn:{partition}:securityhub:{region}::{resource}'.format(
+            partition='aws',
+            region=region,
+            resource=standard_resource.spit(':').pop()
+        )
